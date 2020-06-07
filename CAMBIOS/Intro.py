918e05830db66243c899cb67cbc5c6c7cfa6aa61
Ceratoide <66323471+Ceratoide@users.noreply.github.com>
2020-06-06 22:47:09 -0500

    Arreglando lo que arruiné
    Esta igual que en la actualizacion del main

--- CAMBIOS/Intro.py
+++ CAMBIOS/Intro.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import pygame,sys
 from pygame.locals import *
 import numpy as np
@@ -29,35 +28,3 @@ def Iniciar():
                         
             pygame.display.update() 
 
-=======
-import pygame,sys
-from pygame.locals import *
-import numpy as np
-from Funciones import *
-from Pantalla import *
-from Menu import *
-
-def Iniciar():
-        pygame.init()
-        pygame.display.set_caption("Simulador Campo Electrico ")
-        clock=pygame.time.Clock()
-        
-        screen = pygame.display.set_mode((800, 600))
-        bg_image = pygame.image.load("fondo-pared-ladrillos.jpg")
-        bg_image = bg_image.convert()
-        screen.blit(bg_image,(0,0))
-        clock.tick(10)    
-        otra_pantalla = True
-        while otra_pantalla:
-            for event in pygame.event.get():
-                if event.type==QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_p:
-                        otra_pantalla = False
-                        world.visual()
-                        
-            pygame.display.update() 
-
->>>>>>> Stashed changes
