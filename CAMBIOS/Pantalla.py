5dca15486ed9fad863b2c8d1dd245c1c916b5e3f
Odioaki <66315165+Odioaki@users.noreply.github.com>
2020-06-06 21:25:24 -0500

    Correccion del main
    Ahora el programa se inicia con una pantalla y al presionar p empieza la simulación

--- CAMBIOS/Pantalla.py
+++ CAMBIOS/Pantalla.py
@@ -1,6 +1,8 @@
 import pygame,sys
 from pygame.locals import *
 import numpy as np
+from Funciones import *
+
 class world:
     def __init__(self,ball,cargas):
         
@@ -33,5 +35,42 @@ class world:
                 self.screen.blit(o.image,o.pos)
         
         pygame.display.flip()
+    def visual():
+        
+        c=ball((500,100),(1,0),20)
+        g=ball((0,200),(10,2),-20)
+        v=ball((0,250),(1.5,0),-5)
+        k=ball((300,400),(0,0),10)
+        e=carga((350,250),-20)
+        f=carga((600,250),-20)
+        l=carga((550,500),50)
+        n=carga((550,100),20)
+        p=[]
+        b=[]
+        for i in range(-200,1000,20):
+            p=p+[carga((i,500),10)]
+            p=p+[carga((i,0),-10)]
+            b=b+[ball((i,400),(0,0),15)]
+            b=b+[ball((i,200),(0,0),-15)]
+        #w=world([v],p)
+        #w=world([g,k,c,v],[e])
+        #w=world(b,[e])
+        w=world(b,[e,f,l,n])
+        #PRUEBA VELOCIDAD ORBITAL
+        PO=ball((0,500),(1.5,0),-5)
+        CO=carga((400,300),40)
+        #w=world([PO],[CO])
+        while True:
+        
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        MENU().otra_pantalla()
+                        #w.update()
+                if event.type==QUIT:
+                    pygame.quit()
+                    sys.exit()
+            w.update()
+
 
 
--- CAMBIOS/main.py
+++ CAMBIOS/main.py
@@ -9,48 +9,27 @@ from Intro import *
 
 
 def main():
-    
-    c=ball((500,100),(1,0),20)
-    g=ball((0,200),(10,2),-20)
-    v=ball((0,250),(1.5,0),-5)
-    k=ball((300,400),(0,0),10)
-    e=carga((350,250),-20)
-    f=carga((600,250),-20)
-    l=carga((550,500),50)
-    n=carga((550,100),20)
-    p=[]
-    b=[]
-    for i in range(-200,1000,20):
-        p=p+[carga((i,500),10)]
-        p=p+[carga((i,0),-10)]
-        b=b+[ball((i,400),(0,0),15)]
-        b=b+[ball((i,200),(0,0),-15)]
-    #w=world([v],p)
-    #w=world([g,k,c,v],[e])
-    #w=world(b,[e])
-    w=world(b,[e,f,l,n])
-    #PRUEBA VELOCIDAD ORBITAL
-    PO=ball((0,500),(1.5,0),-5)
-    CO=carga((400,300),40)
-    #w=world([PO],[CO])
-    while True:
-    
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    MENU().otra_pantalla()
-                    #w.update()
+        pygame.init()
+        pygame.display.set_caption("Simulador Campo Electrico ")
+        clock=pygame.time.Clock()
         
-              
-                    
+        screen = pygame.display.set_mode((800, 600))
+        bg_image = pygame.image.load("fondo-pared-ladrillos.jpg")
+        bg_image = bg_image.convert()
+        screen.blit(bg_image,(0,0))
+        clock.tick(10)    
+        otra_pantalla = True
+        while otra_pantalla:
+            for event in pygame.event.get():
+                if event.type==QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_p:
+                        otra_pantalla = False
+                        world.visual()
+                        
+            pygame.display.update() 
 
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
-        w.update()
-        #MENU().otra_pantalla()
-
-        
-   
 main()
 
